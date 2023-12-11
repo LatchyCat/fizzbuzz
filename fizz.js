d2b997c5c32cfb7c8ea4106382a46f1a368dea8a
oriel <orielsilvajr@gmail.com>
2023-12-11 10:31:20 -0500

    completed

--- fizz.js
+++ fizz.js
@@ -12,5 +12,17 @@
 
 
 let fizzBuzz = (number, target) => {
-
+    for (let i = number; i <= target; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBuzz');
+        } else if (i % 3 === 0) {
+            console.log('Fizz');
+        } else if (i % 5 === 0) {
+            console.log('Buzz');
+        } else {
+            console.log(i);
+        }
+    }
 };
+
+fizzBuzz(1, 100)
